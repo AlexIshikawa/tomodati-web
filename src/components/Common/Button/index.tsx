701071a7b88c008744ae9912dc6397d63d6c94de
AlexIshikawa <alex.ishiikawa@gmail.com>
2024-07-31 20:25:07 -0300

    fix: ajuste estilo

--- src/components/Common/Button/index.tsx
+++ src/components/Common/Button/index.tsx
@@ -6,7 +6,7 @@ const buttonVariants = {
 };
 
 export const Button = ({ children, variant, ...rest }: TButton) => {
-  const base = "px-4 py-2 -skew-x-12 relative primary-button__effect";
+  const base = "px-4 py-2 relative primary-button__effect";
   return (
     <button
       className={cn(base, buttonVariants[variant ?? "primary"])}
